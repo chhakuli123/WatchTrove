24159f8d1fdb6089e97a6c820fa8e1387473fe2c
Chhakuli <chhakulizingare12@gmail.com>
2023-06-16 15:38:56 +0530

    feat: Add path for landing page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
+import { Route, Routes } from "react-router-dom";
+
+import { Home } from "pages";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
