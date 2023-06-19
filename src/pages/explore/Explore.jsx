4063aeb31ad784f55035df5f9c746b54883aeed3
Chhakuli <chhakulizingare12@gmail.com>
2023-06-19 11:02:57 +0530

    feat: Add Explore page

---
+++ src/pages/explore/Explore.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import { VideoContainer } from "components";
+
+const Explore = () => {
+  return <div className="middle-content">
+    <VideoContainer/>
+  </div>;
+};
+
+export { Explore };
