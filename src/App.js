680f1869322f1971b5adb1ee9f5cf77c2f2bca9a
Chhakuli <chhakulizingare12@gmail.com>
2023-06-19 10:32:27 +0530

    feat: Add route for Explore Page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useLocation } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
 
-import { Home } from "pages";
+import { Explore, Home } from "pages";
+import { Footer, Sidebar, Topbar } from "components";
 import "./App.css";
 
 function App() {
+  const { pathname } = useLocation();
+
   return (
-    <div className="App">
+    <div className="App pagewrapper">
+      <Toaster />
+      {pathname !== "/" && <Topbar />}
+      <Sidebar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/explore" element={<Explore />} />
       </Routes>
+      {pathname !== "/" && <Footer />}
     </div>
   );
 }
