f991b66ac3fb8723690ba65040512f2bb7bf1a47
Chhakuli <chhakulizingare12@gmail.com>
2023-06-22 09:22:38 +0530

    feat: Add Search Video Feature

--- src/components/shared/topbar/Topbar.jsx
+++ src/components/shared/topbar/Topbar.jsx
@@ -1,16 +1,41 @@
-import React from "react";
+import React, { useState,useCallback,useRef } from "react";
 import { useNavigate } from "react-router-dom";
+import debounce from "lodash.debounce";
 
 import {
   LightModeOutlinedIcon,
   PersonOutlineOutlinedIcon,
   SearchIcon,
 } from "asset";
+import { Search } from "components";
 import "./topbar.css";
 
 const Topbar = () => {
+  const [searchQuery, setSearchQuery] = useState({ query: "" });
+  const [showSearchResult, setShowSearchResult] = useState(false);
   const navigate = useNavigate();
   
+  const SearchInputHandler = (event) => {
+      if (/^\s/.test(event.target.value)) {
+        setShowSearchResult(false);
+        return;
+      } else {
+        if (event.target.value.length > 0) {
+          setShowSearchResult(true);
+          setSearchQuery((prev) => ({ ...prev, query: event.target.value }));
+        } else {
+          setShowSearchResult(false);
+        }
+      }
+    };
+  
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  const debouncedChangeHandler = useCallback(
+      debounce(SearchInputHandler, 500),
+      []
+    );
+  
+    const inputRef = useRef()
   return (
     <div className="topbar-container">
       <nav className="topbar">
@@ -26,8 +51,15 @@ const Topbar = () => {
         </div>
         <div className="topbar-center">
           <div className="topbar-search">
-            <input type="text" placeholder="Search for videos" />
+            <input ref={inputRef} type="text" placeholder="Search for videos"   onChange={debouncedChangeHandler}  />
             <SearchIcon className="search-icon" />
+             {showSearchResult && (
+              <Search
+                searchQuery={searchQuery}
+                setShowSearchResult={setShowSearchResult}
+                inputRef = {inputRef}
+              />
+            )}
           </div>
         </div>
         <div className="topbar-right">
